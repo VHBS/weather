8e327f878da5e0189ceeca0b90c3bc438a50cd3a
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2023-04-21 20:30:26 -0300

    feat: adding input with autocomplete and get lat and lon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,26 @@
 import '@component/styles/globals.css';
+import { Inter } from 'next/font/google';
+import Head from 'next/head';
 import type { AppProps } from 'next/app';
+import { WeatherContextProvider } from '@component/contexts/WeatherContext';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${inter.style.fontFamily};
+          color: black;
+        }
+      `}</style>
+      <WeatherContextProvider>
+        <Head>
+          <title>Weather</title>
+        </Head>
+        <Component {...pageProps} />
+      </WeatherContextProvider>
+    </>
+  );
 }
